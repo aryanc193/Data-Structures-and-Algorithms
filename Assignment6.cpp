c508acbf67ed3c8d93fea15dc7290db0e8b06955
Aryan Choudhary <112373338+aryanc193@users.noreply.github.com>
2023-05-21 19:47:05 +0530

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,42 +1,41 @@
-/*
-Beginning with an empty binary search tree, Construct binary search tree by inserting the
+/* Beginning with an empty binary search tree, Construct binary search tree by inserting the
 values in the order given. After constructing a binary tree -
 i. Insert new node
 ii. Find number of nodes in longest path from root
 iii. Minimum data value found in the tree
-iv. Change a tree so that the roles of the left and right pointers are swapped at every
-node
-v. Search a value
-*/
+iv. Change a tree so that the roles of the left and right pointers are swapped at every node
+v. Search a value */
 
-#include <iostream>
+#include <iostream> 
 
 struct node
 {
-  int data;
-  node *left, *right
+    int data;
+    node *left, *right;
 };
 
 class BST
 {
-  node *root;
-  
-public:
-  BST()
-  {
-  root = NULL;
-  }
-  void create();
-  node *insert(node *t, int d);
-  int min();
-  int max();
-  int longestPath(node *t);
-  void display(node *t);
-  bool search(int d);
-  node *swap(node *t);
-  friend void menu(node *t);
+    node *root;
+
+    public:
+    BST()
+    {
+        root = NULL;
+    }
+    void create();
+    node *insert(node *t, int d);
+    void display(node *t);
+    int min();
+    int max();
+    bool search(int d);
+    node *swap(node *t);
+    int longestPath(node *t);
+    friend void menu(BST tree);
 };
 
-void create()
+int main()
 {
-  
+    
+    return 0;
+}
